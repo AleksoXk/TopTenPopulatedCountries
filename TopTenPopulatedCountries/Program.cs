3b83e42f3e9197a6ece0ed2efe6d0cba90d98157
Owen Thomas <Owen.Thomas@cardiff.gov.uk>
2019-08-12 11:00:04 +0100

    Add project files.

---
+++ TopTenPopulatedCountries/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopTenPopulatedCountries;
+
+namespace Pluralsight.BegCShCollections.TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\c775434\Documents\TopTenPopulatedCountries\TopTenPopulatedCountries\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
+
+
